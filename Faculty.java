42ef30d7a0b18ed7d2be673e60918f47610ca16c
damodar670 <42474196+damodar670@users.noreply.github.com>
2019-01-03 21:56:12 +0530

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -2,82 +2,105 @@ package library;
 import java.awt.*;
 import java.sql.*;
 import java.awt.event.*;
-public class Completes extends Frame implements ActionListener {
-Button b1,b2;
-Label l1,l2,l3;
-TextField t1,t2;
-String s4;
-int b,d;
-Completes(String h,int a,int c)
-{setFont(new Font("serif",Font.BOLD,18));
-	s4=h;
-	b=a;
-	d=c;
-	setLayout(null);
-	l1=new Label("bookid");
-	l2=new Label("Fine");
-	t1=new TextField(30);
-	t2=new TextField(30);
-	t1.setEditable(false);
-	t2.setEditable(false);
-	t1.setText(Integer.toString(b));
-	t2.setText(Integer.toString(d));
-	l1.setBounds(100,100,60,30);
-	l2.setBounds(100,170,60,30);
-	t1.setBounds(170,100,60,30);
-	t2.setBounds(170,170,60,30);
-	b1=new Button("complete");
-	b2=new Button("Exit");
-	add(l1); add(l2); add(t1); add(t2); add(b1);add(b2);
-	b1.setBounds(150,300,60,30);
-	b2.setBounds(220,300,60,30);
-	 b1.addActionListener(this);
-	    b2.addActionListener(this);
-	    addWindowListener(new WindowAdapter()
-	    		{
-	    	      public void windowClosing(WindowEvent e)
-	    	      {
-	    	    	  System.exit(0);
-	    	      }
-	    		});
-	
-}
-public void actionPerformed(ActionEvent e)
-{
-	Connection con=null;
-	 String s2=null;
-	if(b2==e.getSource())
-	{
-		System.exit(0);
-	}
-	if(b1==e.getSource())
-	{
-		  try {
-			 String s6="avail";
-			  Class.forName("com.mysql.jdbc.Driver");
- 			  con=DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb"+"?useSSL=false","root","dlndamu");
-			   PreparedStatement statement12 = con.prepareStatement("select noBookIssued from faculty where fid="+Integer.parseInt(s4)+";"); 
-			   ResultSet result1=statement12.executeQuery();
-				  while(result1.next())
+public class Faculty extends Frame implements ActionListener {
+	 Button b1;
+	  Label l1,l2;
+	  TextField t1,t2;
+	  Label l3,l4;
+	  Faculty()
+	  {setFont(new Font("serif",Font.BOLD,18));
+		  setLayout(null);
+		  b1=new Button("Login");
+		  l1=new Label("userid");
+		  l2=new Label("password");
+		  l3=new Label("");
+		  l4=new Label("");
+	      t1=new TextField(30);
+		  t2=new TextField(30);
+		  t2.setEchoChar('*');
+		  b1.setBounds(400,450,50,20);
+		  l1.setBounds(350,350,100,20);
+		  l2.setBounds(350,390,100,20);
+		  t1.setBounds(420,350,100,20);
+		  t2.setBounds(420,390,100,20);
+		  add(l1);
+		  add(l2);
+		  add(t1);
+		  add(t2);
+		  add(b1);
+		  b1.addActionListener(this);
+		  this.addWindowListener(new WindowAdapter()
+			{
+		      public void windowClosing(WindowEvent e)
+		      {
+		    	  System.exit(0);
+		      }
+			});
+	  }
+	public void actionPerformed(ActionEvent e)
+		{
+		
+			  String s,s1,s3 = null;
+				ResultSet s2 = null;
+				Connection con=null;
+				  if(b1==e.getSource())
 				  {
-					s2=result1.getString(1);
-				  }  
-				  int d1=Integer.parseInt(s2);
-				  d1=d1-1;
-			 PreparedStatement statement11 = con.prepareStatement("update faculty set  noBookIssued= '"+d1+"'where fid= "+Integer.parseInt(s4)+";");
-			 statement11.executeUpdate();
-			 PreparedStatement statement13 = con.prepareStatement("update books set status= '"+s6+"',fid=null"+",dateofp=null"+" where bookid="+b+";");
-			 statement13.executeUpdate();
-			 PreparedStatement statement14 = con.prepareStatement("delete from transaction where fid="+Integer.parseInt(s4)+" and bookid="+b+";");
-			 statement14.executeUpdate();
-			 System.exit(0); 
-		  }
-		  catch (SQLException | ClassNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+					  s=t1.getText();
+					  s1=t2.getText();
+					  try {
+						Class.forName("com.mysql.jdbc.Driver");
+					} catch (ClassNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} 
+					  try {
+						Connection con1=DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb"+"?useSSL=false","root","dlndamu");
+						 con=con1;
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}//instead of testdb it was timeline
+					  
+					PreparedStatement statement = null;
+					try {
+						statement = con.prepareStatement("select password from faculty where fid= '"+Integer.parseInt(s)+"';");
+					} catch (SQLException e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
+			         try {
+						
+			        	 ResultSet result=statement.executeQuery();
+						  
+						  while(result.next())
+						  {
+							  s3=result.getString(1);
+						  }
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+					}
+			      
+			        if(s3.equals(s1))
+			        {
+			        
+			          l3.setText("login successful and proceed");
+			          l3.setBounds(400,500,200,30);
+			          this.add(l3);
+			          Fd c=new Fd(s);
+			          c.setSize(1000,1000);
+			          c.setVisible(true);
+			          c.setTitle("details");
+			          c.setBackground(Color.cyan);
+			       
+			        }
+			        else
+			        {
+			        	 l3.setText("");
+			        	l4.setText("invalid Login");
+			        	 l4.setBounds(400,500,100,20);
+			        	 this.add(l4);
+			        }
+			       
+				  }
+				}
 			}
-		    System.exit(0);
-	}
-}
-}
-
